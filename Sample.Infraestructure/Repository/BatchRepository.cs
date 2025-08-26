b8900149538b9687516428aceebe37e6a72360f7
Leonardor08 <leonardor.dev08@gmail.com>
2025-08-26 13:08:05 -0500

    fixed: laurita estadpo

--- Sample.Infraestructure/Repository/BatchRepository.cs
+++ Sample.Infraestructure/Repository/BatchRepository.cs
@@ -31,7 +31,7 @@ public class BatchRepository<T> : IBatchRepository<T> where T : class
         const string query  = @"INSERT INTO Users (Id, Name, Email, Phone, Status_Id, CreatedDate, UpdateDate) " +
                               "VALUES (:Id, :Name, :Email, :Phone, :Status_Id, :CreatedDate, :UpdateDate)";
 
-        var a = "laurita la mejor :) ";
+        var a = "laurita esta triste :( ";
 
         using OracleCommand command = _oracleContext.CreateCommand(query);
         command.CommandText = query;

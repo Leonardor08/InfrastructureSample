24514e304153237a026df0b6a329c2cadc2d398f
leonardo <leonardor.dev08@gmail.com>
2025-11-13 23:09:20 -0500

    conflict solved

--- Sample.Domain/Models/Test.cs
+++ Sample.Domain/Models/Test.cs
@@ -2,6 +2,10 @@
 {
     public class Test
     {
+<<<<<<< HEAD
+=======
+        public int Testing { get; set; }
+>>>>>>> test3
         public int MyProperty { get; set; }
         public int TestProperty { get; set; }
     }

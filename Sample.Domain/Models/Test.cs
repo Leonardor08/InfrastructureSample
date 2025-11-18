bffa2fb8be5831ca9971996d9ac865ecb8360de5
leonardo <leonardor.dev08@gmail.com>
2025-11-17 22:41:56 -0500

    commit 3 merege

--- Sample.Domain/Models/Test.cs
+++ Sample.Domain/Models/Test.cs
@@ -10,6 +10,6 @@
         public int MyProperty3 { get; set; }
         public int MyProperty4 { get; set; }
         public int MyProperty5 { get; set; }
-        public int MyProperty6 { get; set;
+        public int MyProperty6 { get; set; }
     }
 }

ea5b667a9ea3641da7d63076464a213cae5b6759
leonardo <leonardor.dev08@gmail.com>
2025-11-17 22:40:57 -0500

    commit 1

--- Sample.Domain/Models/Test.cs
+++ Sample.Domain/Models/Test.cs
@@ -9,5 +9,6 @@
         public int MyProperty2 { get; set; }
         public int MyProperty3 { get; set; }
         public int MyProperty4 { get; set; }
+        public int MyProperty5 { get; set;
     }
 }

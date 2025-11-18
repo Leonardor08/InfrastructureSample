6f845f019d6f801a79d4d8ea18a3a9cc55b53777
leonardo <leonardor.dev08@gmail.com>
2025-11-17 22:39:22 -0500

    done

--- Sample.Domain/Models/Test.cs
+++ Sample.Domain/Models/Test.cs
@@ -6,5 +6,8 @@
         public int MyProperty { get; set; }
         public int TestProperty { get; set; }
         public int MyProperty1 { get; set; }
+        public int MyProperty2 { get; set; }
+        public int MyProperty3 { get; set; }
+        public int MyProperty4 { get; set; }
     }
 }

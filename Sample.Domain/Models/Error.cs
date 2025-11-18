1d32638c46f400bde2079d1ee15bb5ec85a987db
leonardo <leonardor.dev08@gmail.com>
2025-11-17 22:45:46 -0500

    test sqash

--- Sample.Domain/Models/Error.cs
+++ Sample.Domain/Models/Error.cs
@@ -5,4 +5,5 @@ public class Error
     public string Code { get; set; } = string.Empty;
     public string Message { get; set; } = string.Empty;
     public int MyProperty { get; set; }
+    public int MyProperty2 { get; set; }
 }
